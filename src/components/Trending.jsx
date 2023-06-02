d53ce6476c09eedded4b1088d4c57a61049394c7
Alireza Rastineh <kouroshrastineh95@gmail.com>
2023-06-02 12:26:19 +0200

    adding details page also for trending coins

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Trending = () => {
   const [trending, setTrending] = useState([]);
@@ -16,30 +17,32 @@ const Trending = () => {
     <div className="rounded-div my-12 py-8 text-primary">
       <h1 className="text-2xl font-bold py-4">Trending Coins</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap4">
-        {trending.map((coin, index) => (
+        {trending.map((coin) => (
           <div
-            key={index}
+            key={coin.item.id}
             className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300"
           >
             <div className="flex w-full items-center justify-between">
-              <div className="flex">
-                <img
-                  className="mr-4 rounded-full"
-                  src={coin.item.small}
-                  alt=""
-                />
-                <div>
-                  <p className="font-bold">{coin.item.name}</p>
-                  <p>{coin.item.symbol}</p>
+              <Link to={`/coin/${coin.item.id}`}>
+                <div className="flex">
+                  <img
+                    className="mr-4 rounded-full"
+                    src={coin.item.small}
+                    alt=""
+                  />
+                  <div>
+                    <p className="font-bold">{coin.item.name}</p>
+                    <p>{coin.item.symbol}</p>
+                  </div>
                 </div>
-              </div>
+              </Link>
               <div className="flex items-center">
                 <img
                   className="w-4 mr-2"
                   src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
                   alt="/"
                 />
-                <p>{coin.item.price_btc.toFixed(12)}</p>
+                <p>{coin.item.price_btc.toFixed(11)}</p>
               </div>
             </div>
           </div>
